da28036a72b05ce56f97e7a61dfe1ff89266ba43
ilya bondarev <proghtmll@gmail.com>
2024-01-26 19:19:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$('.sliders').slick({
+    // dots: true,
+    // infinite: true,
+    // speed: 500,
+    // slidesToShow: 1,
+    // centerMode: true,
+    // variableWidth: true
+    autoplay: true,
+    dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+});
